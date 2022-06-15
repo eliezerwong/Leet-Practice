acbfea79f1a73fa520a579e50077327ff7edcfbe
eliezerwong <70494072+eliezerwong@users.noreply.github.com>
2022-06-15 16:16:48 -0500

    Time: 155 ms (5.23%), Space: 36.7 MB (55.56%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.cs
@@ -0,0 +1,27 @@
+public class Solution {
+    public bool IsValid(string s) {
+        
+        Stack<char> stack = new Stack<char>();
+        
+        foreach (var c in s)
+        {
+            if (c == '(')
+            {
+                stack.Push(')');
+            }
+            else if (c == '{')
+            {
+                stack.Push('}');
+            }
+            else if (c == '[')
+            {
+                stack.Push(']');
+            }
+            else if (stack.Count == 0 || stack.Pop() != c)
+            {
+                return false;
+            }
+        }
+        return stack.Count == 0;
+    }
+}
