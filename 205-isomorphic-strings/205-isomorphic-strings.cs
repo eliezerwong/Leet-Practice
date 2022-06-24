d1d159435fc5f355c033aeb125025cd442070f4d
eliezerwong <70494072+eliezerwong@users.noreply.github.com>
2022-06-24 16:46:35 -0500

    Time: 117 ms (45.98%), Space: 37.6 MB (93.01%) - LeetHub

---
+++ 205-isomorphic-strings/205-isomorphic-strings.cs
@@ -0,0 +1,28 @@
+public class Solution {
+    public bool IsIsomorphic(string s, string t) {
+        if(s == null || t == null || s.Length != t.Length)
+        {
+            return false;
+        }
+        Dictionary<char, char> map = new Dictionary<char, char>();
+        for(int i = 0; i < s.Length; i++)
+        {
+            if (map.ContainsKey(s[i]))
+            {
+                if (map[s[i]] != t[i])
+                {
+                    return false;
+                }
+            }
+            else if (map.ContainsValue(t[i]))
+            {
+                return false;
+            }
+            else
+            {
+                map.Add(s[i], t[i]);
+            }
+        }
+        return true;
+    }
+}
