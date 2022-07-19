10d412e2f07bdf5989e25830f99e8a21f55df78e
eliezerwong <70494072+eliezerwong@users.noreply.github.com>
2022-07-19 15:35:50 -0500

    Time: 53 ms (20.70%), Space: 25.3 MB (46.02%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cs
+++ 509-fibonacci-number/509-fibonacci-number.cs
@@ -1,22 +1,5 @@
 public class Solution {
     public int Fib(int n) {
-        if(n < 2)
-        {
-            return n;
-        }
-        
-        
-        int prev = 1;
-        int prevPrev = 0;        
-        int result = 0;
-        
-        for(int i = 2; i <= n; i++)
-        {
-            result = prev + prevPrev;
-            prevPrev = prev;
-            prev = result;
-        }
-        
-        return result;
+        return n < 2 ? n : Fib(n-1) + Fib(n-2);
     }
 }
